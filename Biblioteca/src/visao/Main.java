4fdd10c8c4bf8efc24195f667258bc9e0c7dd86c
Ana Camilly Guedes <90116902+anacamilly@users.noreply.github.com>
2023-02-01 15:40:23 -0300

    Estrutura

--- Biblioteca/src/visao/Main.java
+++ Biblioteca/src/visao/Main.java
@@ -17,10 +17,10 @@ public class Main {
                     + "|--------------------------------------------------------|\n"
                     + "| ESCOLHA UMA OPÇÃO ABAIXO:                              |\n"
                     + "|--------------------------------------------------------|\n"
-                    + "| 1 - LIVRO                                              |\n"
-                    + "| 2 - LEITOR                                             |\n"
-                    + "| 3 - FUNCIONÁRIO                                        |\n"
-                    + "| 4 - EMPRÉSTIMO                                         |\n"
+                    + "| 1 - LIVROS                                             |\n"
+                    + "| 2 - LEITORES                                           |\n"
+                    + "| 3 - FUNCIONÁRIOS                                       |\n"
+                    + "| 4 - EMPRÉSTIMOS                                        |\n"
                     + "| 5 - SAIR DO SISTEMA                                    |\n"
                     + "|--------------------------------------------------------|\n");
 
@@ -31,14 +31,14 @@ public class Main {
                     do {
                         teclado.nextLine();
                         System.out.println("\n|--------------------------------------------------------|\n"
-                                + "|                       MENU DE LIVRO                    |\n"
+                                + "|                       MENU DE LIVROS                   |\n"
                                 + "|--------------------------------------------------------|\n"
                                 + "| 1 - CADASTRAR                                          |\n"
                                 + "| 2 - ALTERAR                                            |\n"
                                 + "| 3 - BUSCAR                                             |\n"
                                 + "| 4 - REMOVER                                            |\n"
                                 + "| 5 - LISTAR                                             |\n"
-                                + "| 6 - SAIR DO MENU DE LIVRO                              |\n"
+                                + "| 6 - SAIR DO MENU DE LIVROS                             |\n"
                                 + "|--------------------------------------------------------|\n");
                         digitoLivro = teclado.nextInt();
                         teclado.nextLine();
@@ -67,14 +67,14 @@ public class Main {
 		     do {
                         teclado.nextLine();
                         System.out.println("\n|--------------------------------------------------------|\n"
-                                + "|                       MENU DE LEITOR                   |\n"
+                                + "|                      MENU DE LEITORES                  |\n"
                                 + "|--------------------------------------------------------|\n"
                                 + "| 1 - CADASTRAR                                          |\n"
                                 + "| 2 - ALTERAR                                            |\n"
                                 + "| 3 - BUSCAR                                             |\n"
                                 + "| 4 - REMOVER                                            |\n"
                                 + "| 5 - LISTAR                                             |\n"
-                                + "| 6 - SAIR DO MENU DE LEITOR                             |\n"
+                                + "| 6 - SAIR DO MENU DE LEITORES                           |\n"
                                 + "|--------------------------------------------------------|\n");
                         digitoLeitor = teclado.nextInt();
                         teclado.nextLine();
@@ -102,14 +102,14 @@ public class Main {
                     do {
                         teclado.nextLine();
                         System.out.println("\n|--------------------------------------------------------|\n"
-                                + "|                    MENU DE FUNCIONÁRIO                 |\n"
+                                + "|                    MENU DE FUNCIONÁRIOS                |\n"
                                 + "|--------------------------------------------------------|\n"
                                 + "| 1 - CADASTRAR                                          |\n"
                                 + "| 2 - ALTERAR                                            |\n"
                                 + "| 3 - BUSCAR                                             |\n"
                                 + "| 4 - REMOVER                                            |\n"
                                 + "| 5 - LISTAR                                             |\n"
-                                + "| 6 - SAIR DO MENU DE FUNCIONÁRIO                        |\n"
+                                + "| 6 - SAIR DO MENU DE FUNCIONÁRIOS                       |\n"
                                 + "|--------------------------------------------------------|\n");
                         digitoFuncionario = teclado.nextInt();
                         teclado.nextLine();
@@ -131,6 +131,41 @@ public class Main {
 			}
 			    
 		    } while (digitoFuncionario != 6);
+			  break;
+		case 4:
+                    int digitoEmprestimo;
+                    do {
+                        teclado.nextLine();
+                        System.out.println("\n|--------------------------------------------------------|\n"
+                                + "|                    MENU DE EMPRÉSTIMOS                 |\n"
+                                + "|--------------------------------------------------------|\n"
+                                + "| 1 - CADASTRAR                                          |\n"
+                                + "| 2 - ALTERAR                                            |\n"
+                                + "| 3 - BUSCAR                                             |\n"
+                                + "| 4 - REMOVER                                            |\n"
+                                + "| 5 - LISTAR                                             |\n"
+                                + "| 6 - SAIR DO MENU DE EMPRÉSTIMOS                        |\n"
+                                + "|--------------------------------------------------------|\n");
+                        digitoEmprestimo = teclado.nextInt();
+                        teclado.nextLine();
+
+                        switch (digitoEmprestimo) {
+                            	case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+				default:
+			}
+			    
+		    } while (digitoEmprestimo != 6);
 			  break;
             }
             
