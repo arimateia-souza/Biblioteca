c5f4714934091361bf191c4d81077535d5e26119
Ana Camilly Guedes <90116902+anacamilly@users.noreply.github.com>
2023-02-01 15:29:35 -0300

    Update Main.java

--- Biblioteca/src/visao/Main.java
+++ Biblioteca/src/visao/Main.java
@@ -64,7 +64,7 @@ public class Main {
 			    
 		case 2:
 		     int digitoLeitor;
-			     do {
+		     do {
                         teclado.nextLine();
                         System.out.println("\n|--------------------------------------------------------|\n"
                                 + "|                       MENU DE LEITOR                   |\n"
@@ -97,7 +97,41 @@ public class Main {
 			    
 		    } while (digitoLeitor != 6);
 			    break;
+		case 3:
+                    int digitoFuncionario;
+                    do {
+                        teclado.nextLine();
+                        System.out.println("\n|--------------------------------------------------------|\n"
+                                + "|                    MENU DE FUNCIONÁRIO                 |\n"
+                                + "|--------------------------------------------------------|\n"
+                                + "| 1 - CADASTRAR                                          |\n"
+                                + "| 2 - ALTERAR                                            |\n"
+                                + "| 3 - BUSCAR                                             |\n"
+                                + "| 4 - REMOVER                                            |\n"
+                                + "| 5 - LISTAR                                             |\n"
+                                + "| 6 - SAIR DO MENU DE FUNCIONÁRIO                        |\n"
+                                + "|--------------------------------------------------------|\n");
+                        digitoFuncionario = teclado.nextInt();
+                        teclado.nextLine();
+
+                        switch (digitoFuncionario) {
+                            	case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+				default:
+			}
 			    
+		    } while (digitoFuncionario != 6);
+			  break;
             }
             
         }while(digito != 5);
